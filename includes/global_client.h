c3af895c76be5c11f305dcfc2d7a8a1870d5d84c
Jan <molenaarjana@gmail.com>
2023-07-21 09:38:34 +0200

    Fri Jul 21 07:35:57 fixed norm

--- includes/global_client.h
+++ includes/global_client.h
@@ -6,14 +6,14 @@
 /*   By: jmolenaa <jmolenaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/17 22:20:13 by jmolenaa      #+#    #+#                 */
-/*   Updated: 2023/02/17 22:22:40 by jmolenaa      ########   odam.nl         */
+/*   Updated: 2023/07/21 09:31:48 by jmolenaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GLOBAL_CLIENT_H
 # define GLOBAL_CLIENT_H
 
-# include <signal.h> 
+# include <signal.h>
 
 volatile sig_atomic_t	g_flag;
 
--- includes/global_server.h
+++ includes/global_server.h
@@ -6,14 +6,14 @@
 /*   By: jmolenaa <jmolenaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/17 23:14:41 by jmolenaa      #+#    #+#                 */
-/*   Updated: 2023/02/17 23:15:12 by jmolenaa      ########   odam.nl         */
+/*   Updated: 2023/07/21 09:31:50 by jmolenaa      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GLOBAL_SERVER_H
 # define GLOBAL_SERVER_H
 
-# include <signal.h> 
+# include <signal.h>
 
 volatile sig_atomic_t	g_flag;
 
